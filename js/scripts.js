5da741efdb8dafb45718ef5f703bb77748c5215b
Michael Watts <Wattsjmichael@gmail.com>
2020-06-02 10:58:06 -0700

    Adds toggle and slide down to walrus.img
    Co-Authored-by JohnNils Olson johnnils@gmail.com

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,7 @@ $(document).ready(function() {
   $(".clickable").click(function() {
     $("#walrus-showing").fadeToggle(1000);
     $("#walrus-hidden").fadeToggle(1000);
+    $("#togglefont").fadeToggle(500);
+    $("#walrus-showing").slideDown(500);
   });
 });
